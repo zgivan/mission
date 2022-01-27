93cc70dbe17b4ea6b6ada76ae0e0b0a59c2ce764
zg <849190334@qq.com>
2022-01-27 18:01:58 +0800

    123

---
+++ store/modules/login.js
@@ -0,0 +1,37 @@
+export default {
+	state:{
+		userinfo:false,
+		userid: 0,
+		loginStatus:false
+	},
+	mutations:{
+		// 初始化数据
+		initUser(state){
+			let userinfo = uni.getStorageSync('userinfo')
+			if (userinfo) {
+				userinfo = JSON.parse(userinfo)
+				
+				state.userinfo = userinfo
+				state.userid = userinfo.id
+				state.loginStatus = true
+			}
+		},
+		// 登录
+		login(state,userinfo){
+			state.userinfo = userinfo
+			state.userid = userinfo.id
+			state.loginStatus = true
+			uni.setStorageSync('userinfo',JSON.stringify(userinfo))
+		},
+		// 退出
+		logout(state){
+			state.userinfo = false
+			state.userid = 0
+			state.loginStatus = false
+			uni.removeStorageSync('userinfo')
+		}
+	},
+	actions:{
+		
+	}
+}
